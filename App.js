dbe9713ccef1b4a724e0c7dd4b661f2bb02f9a18
Bruno Capobianco <bruno.capobianco@outlook.it>
2024-03-25 14:06:59 +0100

    ToDoList
    added todolist function, delete all element of the list

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [input, setinput] = useState("");
+  const [reachedGoals, setreachedGoals] = useState([]);
+  const goalImputHandler = (enteredText) => {
+    setinput(enteredText);
+  };
+  const addGoalHandler = () => {
+    setreachedGoals((currentReachedGoals) => [...currentReachedGoals, input]);
+  };
+  const deleteAllGoals=()=>{
+    setreachedGoals("")
+  }
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.TextInput} placeholder="risultati" />
-        <Button title="aggiungi" />
+        <TextInput
+          style={styles.TextInput}
+          placeholder="risultati"
+          onChangeText={goalImputHandler}
+        />
+        <Button title="aggiungi" onPress={addGoalHandler} />
       </View>
-      <View style={styles.Text}>
-        <Text>I tuoi risultati</Text>
+      <View style={styles.goals}>
+        {reachedGoals ? (
+        reachedGoals.map((goal, index) => (
+          <Text key={index}>{goal}</Text>
+        ))):''}
+      </View>
+      <View>
+        <Button style={styles.delete} onPress={deleteAllGoals} title="cancella lista"/>
       </View>
     </View>
   );
@@ -19,13 +41,12 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 12,
-    marginTop: "10%",
   },
   inputContainer: {
-    flex: 1,
+    flex: 0.3,
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems:'center',
+    alignItems: "center",
     borderBottomWidth: 1,
     borderBottomColor: "black",
     marginBottom: 24,
@@ -37,8 +58,11 @@ const styles = StyleSheet.create({
     marginRight: 8,
     padding: 8,
   },
-  Text: {
-    flex: 4,
-    
+  goals: {
+    flex: 0.6,
   },
+  delete:{
+    flex:0.2
+  }
+
 });
