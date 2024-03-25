5dce06d8bbef7af6eb2081b2c8978dc5fae84869
Bruno Capobianco <bruno.capobianco@outlook.it>
2024-03-25 12:55:30 +0100

    Update App.js
    First layout

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>ciao matteo!!!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.TextInput} placeholder="risultati" />
+        <Button title="aggiungi" />
+      </View>
+      <View style={styles.Text}>
+        <Text>I tuoi risultati</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 12,
+    marginTop: "10%",
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems:'center',
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+    marginBottom: 24,
+  },
+  TextInput: {
+    borderWidth: 1,
+    borderColor: "black",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  Text: {
+    flex: 4,
+    
   },
 });
